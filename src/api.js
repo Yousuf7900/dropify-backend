cd09177952b949a27bfae48591331580e25097ad
Yousuf7900 <yousufislam7900@gmail.com>
2026-02-09 18:14:22 +0600

    jwt token api geenrated

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-
+const jwt = require('jsonwebtoken');
 const { getCollections } = require('./collections');
 
 const setUpAPI = (app) => {
@@ -6,10 +6,61 @@ const setUpAPI = (app) => {
 
     // test api
     app.get('/api/test', (req, res) => {
-        res.send("API's are working");
+        res.send("Test Api is Running");
     })
 
-    
+    // API's Start From Here
+
+    // JWT related api's here
+    app.post('/jwt', async (req, res) => {
+        const userInfo = req.body;
+        const token = jwt.sign(userInfo, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h', issuer: "Dropify" });
+        res.send({ token });
+    })
+
+
+    // users related all api's here
+    app.patch('/users', async (req, res) => {
+        const userInfo = req.body;
+
+        if (!userInfo?.email || !userInfo?.uid) {
+            return res.status(400).send({ message: "email and uid are required" });
+        }
+
+        const filter = { email: userInfo.email };
+
+        const updateDoc = {
+            $set: {
+                name: userInfo.name,
+                photoURL: userInfo.photoURL,
+                lastLoginAt: userInfo.lastLoginAt,
+                uid: userInfo.uid
+            },
+            $setOnInsert: {
+                role: userInfo.role || 'user',
+                createdAt: userInfo.createdAt,
+                email: userInfo.email
+            }
+        };
+
+        const options = { upsert: true };
+        const result = await usersCollection.updateOne(filter, updateDoc, options);
+        res.send(result);
+    });
+
+
+
+
+    // products related all api's here
+
+
+
+
+    // review related all api's here
+
+
+
+
 };
 
 module.exports = { setUpAPI };
